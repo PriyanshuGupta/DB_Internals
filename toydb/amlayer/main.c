003fd2f0e5ae7772652b12a114a60fa04fedb6dd
Krishna Harsha <krishna.harsha.reddy@gmail.com>
2015-10-26 18:28:10 +0530

    xxx

--- toydb/amlayer/main.c
+++ toydb/amlayer/main.c
@@ -31,41 +31,6 @@ int testval;
 	
 	AM_BulkLoad("file",fd);
 	//~ AM_IndexLoad("file",fd);
-	//~ AM_PrintTree(fd,0,'i'); 
-	//~ int x,y;
-	//~ scanf("%d",&x);
-	//~ AM_PrintTree(fd,0,'i');
-	//~ printf("X \n");
-	//~ scanf("%d",&y); 
-	numrec= 0;
-	sd = AM_OpenIndexScan(fd,INT_TYPE,sizeof(int),EQ_OP,NULL);
-	while(numrec<1576 && (recnum=AM_FindNextEntry(sd))>= 0){
-		//~ printf("%d\n",recnum);
-		numrec++;
-	}
-	printf("retrieved %d records\n",numrec);
-	AM_CloseIndexScan(sd);
-
-
-	numrec= 0;
-	sd = AM_OpenIndexScan(fd,INT_TYPE,sizeof(int),EQ_OP,NULL);
-	while((recnum=AM_FindNextEntry(sd))>= 0){
-		//~ printf("%d\n",recnum);
-		numrec++;
-	}
-	printf("retrieved %d records\n",numrec);
-	AM_CloseIndexScan(sd);
-//~ 
-	/* print out what remains */
-   //~ printf("printing between 100 and 150\n");
-	numrec= 0;
-	sd = AM_OpenIndexScan(fd,INT_TYPE,sizeof(int),EQ_OP,NULL);
-	while((recnum=AM_FindNextEntry(sd))>= 0){
-		//~ printf("%d\n",recnum);
-		numrec++;
-	}
-	printf("retrieved %d records\n",numrec);
-	AM_CloseIndexScan(sd);
 	
 	/* destroy everything */
 	printf("closing down\n");
